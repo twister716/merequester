d2e51e40599f4bee5c0b51de7bd3d2e7e6b14b4e
rlnt <relentless@rlnt.dev>
2024-03-29 12:40:44 +0100

    switch to proper screen registration

--- src/main/java/com/almostreliable/merequester/MERequester.java
+++ src/main/java/com/almostreliable/merequester/MERequester.java
@@ -1,11 +1,18 @@
 package com.almostreliable.merequester;
 
+import appeng.init.client.InitScreens;
+import com.almostreliable.merequester.client.RequesterScreen;
+import com.almostreliable.merequester.client.RequesterTerminalScreen;
 import com.almostreliable.merequester.network.PacketHandler;
+import com.almostreliable.merequester.requester.RequesterMenu;
+import com.almostreliable.merequester.terminal.RequesterTerminalMenu;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import org.slf4j.Logger;
 
 @Mod(BuildConfig.MOD_ID)
@@ -20,7 +27,23 @@ public final class MERequester {
         modEventBus.addListener(Registration::registerCapabilities);
         modEventBus.addListener(Registration.Tab::initContents);
         modEventBus.addListener(PacketHandler::onPacketRegistration);
+        if (FMLEnvironment.dist.isClient()) {
+            modEventBus.addListener(MERequesterClient::registerScreens);
+        }
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
+    }
+
+    private static class MERequesterClient {
+
+        @SuppressWarnings("RedundantTypeArguments")
+        private static void registerScreens(RegisterMenuScreensEvent event) {
+            InitScreens.register(RequesterMenu.TYPE, RequesterScreen::new, String.format("/screens/%s.json", REQUESTER_ID));
+            InitScreens.<RequesterTerminalMenu, RequesterTerminalScreen<RequesterTerminalMenu>> register(
+                RequesterTerminalMenu.TYPE,
+                RequesterTerminalScreen::new,
+                String.format("/screens/%s.json", TERMINAL_ID)
+            );
+        }
     }
 }
