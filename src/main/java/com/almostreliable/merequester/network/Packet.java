9e817e4ec63685b1ad031538c2552959eecfbeb9
rlnt <relentless@rlnt.dev>
2024-09-04 16:18:54 +0200

    remove unused packet interface

--- src/main/java/com/almostreliable/merequester/network/Packet.java
+++
@@ -1,9 +0,0 @@
-package com.almostreliable.merequester.network;
-
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.world.entity.player.Player;
-
-public interface Packet extends CustomPacketPayload {
-
-    void handle(Player player);
-}
